1757825f4e6d10cc078657a0c0fbbd895a0ec36c
RettichLP <rettichlp01@gmail.com>
2024-11-16 17:47:06 +0100

    Remove key filter for DoubleKeyPressEvent creator in KeyPressListener and add car relevant methode in DoubleKeyPressEvent

--- core/src/main/java/de/rettichlp/germanrpcaraddon/events/DoubleKeyPressEvent.java
+++ core/src/main/java/de/rettichlp/germanrpcaraddon/events/DoubleKeyPressEvent.java
@@ -1,17 +1,23 @@
 package de.rettichlp.germanrpcaraddon.events;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 import net.labymod.api.client.gui.screen.key.Key;
 import net.labymod.api.event.Event;
 
+import static net.labymod.api.client.gui.screen.key.Key.S;
+import static net.labymod.api.client.gui.screen.key.Key.SPACE;
+import static net.labymod.api.client.gui.screen.key.Key.W;
+
+@Data
+@RequiredArgsConstructor
+@Accessors(fluent = true)
 public class DoubleKeyPressEvent implements Event {
 
     private final Key key;
 
-    public DoubleKeyPressEvent(Key key) {
-        this.key = key;
-    }
-
-    public Key key() {
-        return key;
+    public boolean isCarRelevantKey() {
+        return key == W || key == S || key == SPACE;
     }
 }
--- core/src/main/java/de/rettichlp/germanrpcaraddon/listener/KeyPressListener.java
+++ core/src/main/java/de/rettichlp/germanrpcaraddon/listener/KeyPressListener.java
@@ -8,62 +8,34 @@ import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.input.KeyEvent;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import static java.lang.System.currentTimeMillis;
 import static net.labymod.api.Laby.fireEvent;
 import static net.labymod.api.Laby.references;
-import static net.labymod.api.client.gui.screen.key.Key.S;
-import static net.labymod.api.client.gui.screen.key.Key.SPACE;
-import static net.labymod.api.client.gui.screen.key.Key.W;
 import static net.labymod.api.event.client.input.KeyEvent.State.PRESS;
 
-/**
- * Listener for detecting double key presses on specific keys, designed for use with the GermanRPCarAddon. This listener checks key
- * press events and triggers a double-click event when a key is pressed twice within a short interval.
- *
- * <p>The class maintains a record of the last press times for specific keys
- * ({@code W}, {@code S}, and {@code SPACE}) to detect double presses. If a key is pressed twice within 500 milliseconds, it considers
- * it a double press, logs this action for debugging purposes, and fires a custom {@link DoubleKeyPressEvent}.
- */
 @RequiredArgsConstructor
 public class KeyPressListener {
 
     private final GermanRPCarAddon addon;
 
-    /**
-     * A map holding the keys that are monitored for double-press actions, along with the timestamp of their last press event.
-     */
-    private final HashMap<Key, Long> lastPress = new HashMap<>(Map.of(
-            W, 0L,
-            S, 0L,
-            SPACE, 0L
-    ));
+    private final HashMap<Key, Long> lastPress = new HashMap<>();
 
-    /**
-     * Handles {@link KeyEvent} to detect and respond to double key presses.
-     *
-     * <p>On receiving a key press event, the method first verifies if the key
-     * is being observed. If the key has been pressed twice within a set time window (500 milliseconds), it triggers a
-     * {@link DoubleKeyPressEvent} and resets the recorded press time for that key.
-     *
-     * @param event The {@link KeyEvent} representing a key press event, containing the key and its press state.
-     */
     @Subscribe
     public void onKey(KeyEvent event) {
         Key key = event.key();
 
         // Check if the key is being observed (to save performance) and if the chat is not open
-        if (event.state() != PRESS || !this.lastPress.containsKey(key) || references().chatAccessor().isChatOpen()) {
+        if (event.state() != PRESS || references().chatAccessor().isChatOpen()) {
             return;
         }
 
         // Check for double press, because single press is already used by the server
-        if (currentTimeMillis() - this.lastPress.get(key) < 500) {
+        if (currentTimeMillis() - this.lastPress.getOrDefault(key, 0L) < 500) {
             this.addon.debug("Key double pressed: " + key.getName());
 
             // Reset the time of the last press
-            this.lastPress.put(key, 0L);
+            this.lastPress.remove(key);
 
             // Fire own event for double press
             fireEvent(new DoubleKeyPressEvent(key));
