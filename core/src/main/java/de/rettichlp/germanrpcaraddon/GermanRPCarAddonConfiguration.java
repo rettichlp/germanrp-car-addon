2ea0f3eba2ceb9d62307b71e5689749962482433
RettichLP <rettichlp01@gmail.com>
2024-11-19 22:06:43 +0100

    Add config options and logic

--- core/src/main/java/de/rettichlp/germanrpcaraddon/GermanRPCarAddonConfiguration.java
+++ core/src/main/java/de/rettichlp/germanrpcaraddon/GermanRPCarAddonConfiguration.java
@@ -6,6 +6,7 @@ import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import net.labymod.api.configuration.settings.annotation.SettingSection;
 
 /**
  * The configuration settings for the GermanRP Car Addon.
@@ -20,6 +21,17 @@ public class GermanRPCarAddonConfiguration extends AddonConfig {
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+    @SettingSection("comfort-systems")
+    @SwitchSetting
+    private final ConfigProperty<Boolean> pressToStart = new ConfigProperty<>(true);
+
+    @SwitchSetting
+    private final ConfigProperty<Boolean> automaticGearbox = new ConfigProperty<>(true);
+
+    @SettingSection("assistance-systems")
+    private final ConfigProperty<Boolean> automaticEmergencyBraking = new ConfigProperty<>(true);
+
+    @SettingSection("development")
     @SwitchSetting
     private final ConfigProperty<Boolean> debug = new ConfigProperty<>(false);
 }
